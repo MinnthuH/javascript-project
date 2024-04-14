39372f0c84bb57b15952393eb7cef052a45f6ed2
Beth <minthu.mingalarit@gmail.com>
2024-04-14 22:48:28 +0630

    expense tracker finished

---
+++ Expense-Tracker/app.js
@@ -0,0 +1,53 @@
+const addEdpenseBtn = document.querySelector(".add-expense-btn");
+const expenseList = document.querySelector(".expense-list");
+const totalExpenses = document.querySelector(".total-expense h3");
+
+let expenses = [];
+let total = 0;
+
+// show expense
+function renderExpenses() {
+  let html = "";
+  expenses.forEach((expense) => {
+    html += `<div class="expense-item">
+        <div class="expense-item-description">${expense.description}</div>
+        <div class="expense-item-amount">${expense.amount}</div>
+        <button class="delete-expense-btn">&times;</button>
+        </div>`;
+  });
+
+  expenseList.innerHTML = html;
+  totalExpenses.innerText = `Total Expenses: $${total}`;
+}
+
+// add expense
+function addExpense() {
+  const description = prompt("Enter Expense Description");
+  const amount = parseFloat(prompt("Enter Expense Amount"));
+
+  if (description && amount) {
+    const expense = {
+      description: description,
+      amount: amount,
+    };
+    expenses.push(expense);
+    total += amount;
+    renderExpenses();
+  }
+}
+addEdpenseBtn.addEventListener("click", addExpense);
+
+// delete expense
+function deleteExpense(index) {
+  total -= expenses[index].amount;
+  expenses.splice(index, 1);
+  renderExpenses();
+}
+expenseList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-expense-btn")) {
+    const index = Array.from(e.target.parentNode.parentNode.children).indexOf(
+      e.target.parentNode
+    );
+    deleteExpense(index);
+  }
+});
